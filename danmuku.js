7100828cb27063f67e8ea59322de686a43b70824
753yuva <akalivaty@gmail.com>
2024-09-18 02:38:30 +0800

    1. add speed up/down function 2. modified showFloatingMessage()

--- danmuku.js
+++ danmuku.js
@@ -2,58 +2,58 @@
 
 let danmukuBox;
 
-function toggle_danmukuBox() {
-    window.addEventListener('keydown', function (event) {
-        if (event.key === 'F1') {
-            event.preventDefault();
+function toggleDanmukuBox(event) {
+    if (event.key === 'F1') {
+        event.preventDefault();
 
-            danmukuBox = document.querySelector('#custom-input-box');
-            if (!danmukuBox) {
-                danmukuBox = document.createElement('input');
-                danmukuBox.id = 'custom-input-box';
-                danmukuBox.type = 'text';
-                danmukuBox.placeholder = 'Enter danmuku here';
-                danmukuBox.autocomplete = 'off';
-                danmukuBox.style.display = 'none';
-                danmukuBox.style.position = 'absolute';
-                danmukuBox.style.zIndex = '1000';
-                danmukuBox.style.padding = '10px';
-                danmukuBox.style.fontSize = '16px';
-                danmukuBox.style.width = '500px';
-                danmukuBox.style.borderRadius = '8px';
-                danmukuBox.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+        danmukuBox = document.querySelector('#custom-input-box');
+        if (!danmukuBox) {
+            danmukuBox = document.createElement('input');
+            danmukuBox.id = 'custom-input-box';
+            danmukuBox.type = 'text';
+            danmukuBox.placeholder = 'Enter danmuku here';
+            danmukuBox.autocomplete = 'off';
+            danmukuBox.style.display = 'none';
+            danmukuBox.style.position = 'absolute';
+            danmukuBox.style.zIndex = '1000';
+            danmukuBox.style.padding = '10px';
+            danmukuBox.style.fontSize = '16px';
+            danmukuBox.style.width = '500px';
+            danmukuBox.style.borderRadius = '8px';
+            danmukuBox.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
 
-                document.body.appendChild(danmukuBox);
-            }
-
-            getVideoRect();
-
-            if (danmukuBox.style.display === 'none' || danmukuBox.style.display === '') {
-                console.log('Displaying danmukuBox');
-                danmukuBox.style.setProperty('display', 'block', 'important');
-                danmukuBox.focus();
-                window.addEventListener('keydown', listenDanmuku);
-            } else {
-                console.log('Hiding danmukuBox');
-                danmukuBox.style.display = 'none';
-                document.querySelector('video').focus();
-                window.removeEventListener('keydown', listenDanmuku);
-            }
+            document.documentElement.appendChild(danmukuBox);
         }
-    });
+
+        getVideoRect();
+
+        if (danmukuBox.style.display === 'none' || danmukuBox.style.display === '') {
+            console.log('Displaying danmukuBox');
+            danmukuBox.style.display = 'block';
+            danmukuBox.focus();
+            document.addEventListener('keydown', listenDanmuku);
+        } else {
+            console.log('Hiding danmukuBox');
+            danmukuBox.style.display = 'none';
+            document.querySelector('video').focus();
+            window.removeEventListener('keydown', listenDanmuku);
+        }
+    }
 }
 
 function updateDanmukuBoxPosition() {
-    if(!danmukuBox) {
+    if (!danmukuBox) {
         return;
     }
-    
-    getVideoRect();
+
+    setTimeout(() => {
+        getVideoRect();
+    }, 50);
 
     // Reopen the danmuku box if it was open when the window was resized
     if (danmukuBox.style.display === 'block') {
         danmukuBox.style.display = 'none';
-        danmukuBox.style.setProperty('display', 'block', 'important');
+        danmukuBox.style.display = 'block';
         danmukuBox.focus();
     }
 }
@@ -62,9 +62,12 @@ function getVideoRect() {
     const videoElement = document.querySelector('.videoframe');
     if (videoElement) {
         const videoRect = videoElement.getBoundingClientRect();
-        danmukuBox.style.top = `${videoRect.top + videoRect.height / 2}px`;
-        danmukuBox.style.left = `${videoRect.left + videoRect.width / 2}px`;
+        const vertialOffset = videoRect.top + videoRect.height / 2;
+        const horizontalOffset = videoRect.left + videoRect.width / 2;
+        danmukuBox.style.top = `${vertialOffset}px`;
+        danmukuBox.style.left = `${horizontalOffset}px`;
         danmukuBox.style.transform = 'translate(-50%, -50%)';
+        console.log('vertialOffset:', vertialOffset, '\thorizontalOffset:', horizontalOffset);
     } else {
         console.error('Video element not found');
     }
--- utils.js
+++ utils.js
@@ -19,17 +19,20 @@ function filterPage(URL_PATTERNS, DEFAULT_SETTINGS) {
     const current_url = window.location.href;
 
     if (URL_PATTERNS.HOME_PAGE.test(current_url)) {
+        // If homepage
         console.log("here is homepage");
+
         if (GM_getValue('autoExpandMenu', DEFAULT_SETTINGS.autoExpandMenu)) {
             triggerShowMoreButton();
         }
     } else if (URL_PATTERNS.VIDEO_PAGE.test(current_url) || URL_PATTERNS.PARTY_PAGE.test(current_url)) {
+        // If video page
         console.log("here is video page, " + current_url);
 
         if (GM_getValue('enableCenteredDanmukuBox', DEFAULT_SETTINGS.enableCenteredDanmukuBox)) {
             document.addEventListener('fullscreenchange', updateDanmukuBoxPosition);
-
-            toggle_danmukuBox();
+            document.addEventListener('keydown', toggleDanmukuBox);
+            document.addEventListener('keydown', modifySpeed);
         }
 
     } else {
@@ -46,13 +49,40 @@ function triggerShowMoreButton() {
     }
 }
 
-function showFloatingMessage(message) {
+function modifySpeed(event) {
+    const video = document.querySelector('video');
+
+    const speeds = [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2];
+    let currentSpeedIndex = speeds.indexOf(video.playbackRate);
+
+    if (event.shiftKey && event.key === '>') {
+        // speed up
+        if (currentSpeedIndex < speeds.length - 1) {
+            currentSpeedIndex++;
+            video.playbackRate = speeds[currentSpeedIndex];
+            showFloatingMessage(video.playbackRate + "X", 500, 'bottom-right');
+        }
+    } else if (event.shiftKey && event.key === '<') {
+        // speed down
+        if (currentSpeedIndex > 0) {
+            currentSpeedIndex--;
+            video.playbackRate = speeds[currentSpeedIndex];
+            showFloatingMessage(video.playbackRate + "X", 500, 'bottom-right');
+        }
+    }
+}
+
+function showFloatingMessage(message, duration = 2000, position = 'bottom-center') {
+
+    const existingMsg = document.getElementById('floatingMessage');
+    if (existingMsg) {
+        existingMsg.parentNode.removeChild(existingMsg);
+    }
+
     const msg = document.createElement('div');
+    msg.id = 'floatingMessage';
     msg.textContent = message;
     msg.style.position = 'fixed';
-    msg.style.bottom = '20px';
-    msg.style.left = '50%';
-    msg.style.transform = 'translateX(-50%)';
     msg.style.fontSize = '24px';
     msg.style.fontWeight = 'bold';
     msg.style.padding = '20px';
@@ -63,23 +93,48 @@ function showFloatingMessage(message) {
     msg.style.opacity = '0';
     msg.style.transition = 'opacity 0.3s';
 
+    switch (position) {
+        case 'bottom-center':
+            msg.style.bottom = '20px';
+            msg.style.left = '50%';
+            msg.style.transform = 'translateX(-50%)';
+            break;
+        case 'bottom-right':
+            msg.style.bottom = '20px';
+            msg.style.right = '5%';
+            msg.style.transform = 'none';
+            break;
+        case 'bottom-left':
+            msg.style.bottom = '20px';
+            msg.style.left = '5%';
+            msg.style.transform = 'none';
+            break;
+        default:
+            msg.style.bottom = '20px';
+            msg.style.left = '50%';
+            msg.style.transform = 'translateX(-50%)';
+            break;
+    }
 
-    document.body.appendChild(msg);
 
-    msg.classList.add('fade-in');
+    document.documentElement.appendChild(msg);
+
+    // Trigger the fade-in effect
+    requestAnimationFrame(() => {
+        msg.style.opacity = '1';
+    });
 
     // Trigger the fade-out effect after 2 seconds (2000 ms)
     setTimeout(() => {
-        msg.classList.remove('fade-in');
-        msg.classList.add('fade-out'); // Trigger fade-out effect
-    }, 2000);
+        msg.style.opacity = '0';
+    }, duration);
 
     // Remove the message from the DOM after the fade-out completes
     setTimeout(() => {
         if (msg && msg.parentNode) {
             msg.parentNode.removeChild(msg);
         }
-    }, 2500); // Total duration (fade-in + visible + fade-out)
+    }, duration + 500); // Total duration (fade-in + visible + fade-out)
 }
 
 // function getCSRFToken() {
